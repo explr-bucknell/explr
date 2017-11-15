d6451a95edb2c9ea2f7236da7cfd553b97e2d678
jw057 <jw057@bucknell.edu>
2017-11-15 10:19:49 -0500

    google places search modal

--- pages/MapPage.js
+++ pages/MapPage.js
@@ -12,11 +12,14 @@ import MapView from 'react-native-maps' // eslint-disable-line no-unused-vars
 import Sidebar from '../components/Sidebar' // eslint-disable-line no-unused-vars
 import Toolbar from '../components/Toolbar' // eslint-disable-line no-unused-vars
 import MapMarkerCallout from '../components/MapMarkerCallout'
+import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete'
+import Modal from 'react-native-modalbox'
 
 export default class MapPage extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
+      modalVisible: false,
       sideBarShowing: false,
       region: {
         latitude: 40.9549774,
@@ -74,6 +77,10 @@ export default class MapPage extends React.Component {
     }
   }
 
+  setModalVisible(visible) {
+    this.setState({ modalVisible: visible })
+  }
+
   onRegionChange (region) {
     this.setState({ region })
   }
@@ -104,11 +111,57 @@ export default class MapPage extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Modal position={"bottom"} isOpen={this.state.modalVisible} onClosed={() => this.setState({modalVisible: false})} style={styles.googleSearch}>
+          <GooglePlacesAutocomplete
+            placeholder="Search"
+            minLength={2} // minimum length of text to search
+            autoFocus={false}
+            returnKeyType={'search'} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
+            listViewDisplayed="auto" // true/false/undefined
+            fetchDetails={true}
+            renderDescription={row => row.description} // custom description render
+            onPress={(data, details = null) => {
+              // 'details' is provided when fetchDetails = true
+              console.log(data);
+              console.log(details);
+            }}
+            getDefaultValue={() => {
+              return ''; // text input default value
+            }}
+            query={{
+              // available options: https://developers.google.com/places/web-service/autocomplete
+              key: 'AIzaSyC2QhtACfVZ2cr9HVvxQuzxd3HT36NNK3Q',
+              language: 'en', // language of the results
+              //types: '(cities)', // default: 'geocode'
+            }}
+            styles={{
+              description: {
+                fontWeight: 'bold',
+              },
+              predefinedPlacesDescription: {
+                color: '#1faadb',
+              },
+            }}
+            currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
+            currentLocationLabel="Current location"
+            nearbyPlacesAPI="GooglePlacesSearch" // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
+            GoogleReverseGeocodingQuery={{
+              // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
+            }}
+            GooglePlacesSearchQuery={{
+              // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+              rankby: 'prominence',
+              //types: 'food',
+            }}
+            debounce={200}
+          />
+        </Modal>
         <MapView
           style={styles.map}
           initialRegion={this.state.region}
           onRegionChange={this.onRegionChange.bind(this)}
           onPress={this.hideBarElements.bind(this)}
+          onLongPress={e => this.setModalVisible(true)}
         >
           { this.state.markers.map((marker, index) => (
             <MapView.Marker
@@ -151,5 +204,8 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0
+  },
+  googleSearch: {
+    height: 300
   }
 })
